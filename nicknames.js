5dd15460f84d1036b1139ec7f06269fef6e19adc
adammoftah <adam.moftah@yale.edu>
2016-02-04 19:46:36 -0500

    Update nicknames.js

--- nicknames.js
+++ nicknames.js
@@ -1,4 +1,18 @@
 var nicknames = {
     "sam": "Samuel",
-    "frankie": "Frank"
+    "frankie": "Frank",
+    "ben": "Benjamin",
+    "chris": "Christopher",
+    "jake": "Jacob",
+    "matt": "Matthew",
+    "will": "William",
+    "alex": "Alexander",
+    "alex": "Alexandera",
+    "josh": "Joshua",
+    "kate": "Katherine",
+    "katie": "Katherine",
+    "caity": "Catherine",
+    "tommy": "Thomas",
+    "maddy": "Madeleine",
+    "gabe": "Gabriel"
 }
